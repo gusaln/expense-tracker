a059f77627dd1f07fc6346ebaef3cb043c7790c2
Gustavo López <glopez.2b@gmail.com>
2021-11-14 23:03:19 -0400

    Fix alignment of TransactionGroupHeader

--- client/src/components/TransactionGroupHeader.jsx
+++ client/src/components/TransactionGroupHeader.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function TransactionGroupHeader(props) {
   return (
-    <div className="flex justify-between items-end -mx-2 mb-2 py-2 px-4 bg-gray-200 text-gray-900">
+    <div className="flex justify-between items-end mb-2 py-2 px-4 bg-gray-200 text-gray-900">
       <div className="h-full w-10 text-3xl text-center">
         {props.day.split("-")[2]}
       </div>
